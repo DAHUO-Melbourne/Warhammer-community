6d4e08ddd6461c9d794bfbf1c411f4161710f534
DAHUO-Melbourne <huodamelbourne@gmail.com>
2020-03-04 18:21:01 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Carousel } from 'react-bootstrap';
+import {Header} from './style';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
       <Carousel>
         <Carousel.Item>
           <img
---
+++ src/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Header = styled.div`
+    position: fixed;
+    top:0;
+    left:0;
+    width: 100%;
+    height: 20px;
+    background-color: black;
+    z-index: 100
+`
