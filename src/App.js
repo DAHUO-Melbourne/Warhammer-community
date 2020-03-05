d71e33864dd310fa429be204283ed9ca51435318
DAHUO-Melbourne <huodamelbourne@gmail.com>
2020-03-05 21:06:13 +1100

    feat: add social links

--- src/App.js
+++ src/App.js
@@ -56,8 +56,12 @@ class App extends Component {
     <div className="App">
       <Header>
         <Sociallink>
-          <Socialicon src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png"/>
+          <a href="https://www.instagram.com/warhammerofficial/" target="_blank">
+          <Socialicon src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png" href="https://www.w3schools.com"/>
+          </a>
+          <a href="https://twitter.com/warhammer_art" target="_blank">
           <Socialicon src="https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_Twitter-512.png"/>
+          </a>
         </Sociallink>
         <LogoImage src="https://content.totalwar.com/total-war/com.totalwar.www2019/uploads/2017/07/24103739/TWW2_RGB_logo_optz-300x130.png" />
         <Navlink>
