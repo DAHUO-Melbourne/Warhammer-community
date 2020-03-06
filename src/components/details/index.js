4b164182ed8f5698507c3375f38a52909f808ab4
DAHUO-Melbourne <huodamelbourne@gmail.com>
2020-03-06 23:23:29 +1100

    feat: adding netlify to form

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -12,20 +12,13 @@ class Details extends Component{
         <H1>{this.props.location.query.title}</H1>
         <div>{this.props.location.query.content}</div>
         <iframe width="560" height="315" src={this.props.location.query.src} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <form name="contact" data-netlify="true" netlify action="POST">
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
           <p>
-            <label>Your Name: <input type="text" name="name" /></label>   
+            <label>Your Name: <input type="text" name="name"/></label>
           </p>
           <p>
-            <label>Your Email: <input type="email" name="email" /></label>
-          </p>
-          <p>
-          <label>Your Role: 
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
+            <label>Your Email: <input type="email" name="email"/></label>
           </p>
           <p>
             <label>Message: <textarea name="message"></textarea></label>
