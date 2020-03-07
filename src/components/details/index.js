5348a273762225802b7041892153a83acc09bbfd
DAHUO-Melbourne <huodamelbourne@gmail.com>
2020-03-07 14:43:18 +1100

    feat: adding html netlify form

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -13,28 +13,21 @@ class Details extends Component{
         <H1>{this.props.location.query.title}</H1>
         <div>{this.props.location.query.content}</div>
         <iframe width="560" height="315" src={this.props.location.query.src} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <NetlifyForm name='Contact Form'>
-    {({ loading, error, success }) => (
-      <div>
-        {loading &&
-          <div>Loading...</div>
-        }
-        {error &&
-          <div>Your information was not sent. Please try again later.</div>
-        }
-        {success &&
-          <div>Thank you for contacting us!</div>
-        }
-        {!loading && !success &&
-          <div>
-            <input type='text' name='Name' required />
-            <textarea name='Message' required />
-            <button>Submit</button>
-          </div>
-        }
-      </div>
-    )}
-</NetlifyForm>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>,
       </Fragment>
     )
   }
